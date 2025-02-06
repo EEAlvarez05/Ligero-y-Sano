48efb61c7d253aa85ad8ae70bc628affab53d38d
EmiiA05 <emiialvareez7@gmail.com>
2025-02-06 05:10:14 -0300

    cambios de <links/> a <a/>

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -12,26 +12,26 @@ function Linkss() {
     },[dropDown])
     return (  
         <ul>
-            <li><Link to="/"><i className="fa-solid fa-house"></i>Home</Link></li>
+            <li><a href="/"><i className="fa-solid fa-house"></i>Home</a></li>
             <li><i className="fa-solid fa-user"></i>Sobre Nosotros</li>
             <li className="link--button"><button onClick={ChangeDropDown}><i className="fa-solid fa-book"></i>Recetas
             <i className="fa-solid fa-caret-down"></i></button>
                 <div className="nav__dropdown">
                     <ul>
-                        <li><Link to="/Sopas-y-Entradas">
-                            <i className='bx bxs-bowl-hot'></i>{Info[4]["Title-recipe"]}</Link>
+                        <li><a href="/sopas-y-entradas">
+                            <i className='bx bxs-bowl-hot'></i>{Info[4]["Title-recipe"]}</a>
                         </li>
-                        <li><Link to="/Carnes">
-                            <i className="fa-solid fa-drumstick-bite"></i>{Info[2]["Title-recipe"]}</Link>
+                        <li><a href="/carnes">
+                            <i className="fa-solid fa-drumstick-bite"></i>{Info[2]["Title-recipe"]}</a>
                         </li>
-                        <li><Link to="/Pescados">
-                            <i className="fa-solid fa-fish"></i>{Info[1]["Title-recipe"]}</Link>
+                        <li><a href="/pescados">
+                            <i className="fa-solid fa-fish"></i>{Info[1]["Title-recipe"]}</a>
                         </li>
-                        <li><Link to="/Ensaladas-y-Verduras">
-                            <i className="fa-solid fa-carrot"></i>{Info[3]["Title-recipe"]}</Link>
+                        <li><a href="/ensaladas-y-verduras">
+                            <i className="fa-solid fa-carrot"></i>{Info[3]["Title-recipe"]}</a>
                         </li>
-                        <li><Link to="/Postres">
-                            <i className="fa-solid fa-ice-cream"></i>{Info[0]["Title-recipe"]}</Link>
+                        <li><a href="/postres">
+                            <i className="fa-solid fa-ice-cream"></i>{Info[0]["Title-recipe"]}</a>
                         </li>
                     </ul>
                 </div>
