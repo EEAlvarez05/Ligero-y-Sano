c460e6efd9593e4fdf5f5e3dd58790bb1e11d56f
EmiiA05 <emiialvareez7@gmail.com>
2025-02-05 21:48:04 -0300

    no se arreglo el problema

--- src/components/ButtonMenu.jsx
+++ src/components/ButtonMenu.jsx
@@ -8,7 +8,6 @@ function ButtonMenu() {
     }
     useEffect(() => {
         document.querySelector("body").classList.toggle("nav__active")
-        setNavActivate(false)
     },[navActivate])
     return (  
         <button className="header__menu" onClick={ChangeNav}>
