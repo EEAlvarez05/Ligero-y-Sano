6a3e53604fba63ffc7ebf601120fd46540759fdc
EmiiA05 <emiialvareez7@gmail.com>
2025-02-05 22:35:38 -0300

    nosee

--- src/components/ButtonMenu.jsx
+++ src/components/ButtonMenu.jsx
@@ -1,16 +1,29 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function ButtonMenu() {
-    const [navActivate, setNavActivate] = useState(true)
+    const [navActivate, setNavActivate] = useState(false);
+    const [mounted, setMounted] = useState(false); // Verifica si el componente ya se montó
+
     useEffect(() => {
-        document.querySelector("body").classList.toggle("nav__active")
-    },[navActivate])
+        setMounted(true); // Indica que ya estamos en el cliente
+    }, []);
+
+    useEffect(() => {
+        if (!mounted) return; // Evita ejecutar en el SSR
+
+        if (navActivate) {
+            document.querySelector("body").classList.add("nav__active");
+        } else {
+            document.querySelector("body").classList.remove("nav__active");
+        }
+    }, [navActivate, mounted]);
+
     const ChangeNav = () => {
-        setNavActivate(!navActivate)
-    }
-    return (  
-        <button className="header__menu" onClick={ChangeNav}>
+        setNavActivate((prev) => !prev);
+    };
+
+    return (
+        <button className="headermenu" onClick={ChangeNav}>
             <span className="menu--span">
                 <i className="fa-solid fa-bars"></i>
                 <i className="fa-solid fa-x"></i>
